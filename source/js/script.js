085db74d837325a2f029f545aef7ae8cdcb95cd6
Kristina Vuychich <kozetta2004@yandex.ru>
2021-03-12 22:40:30 +0300

    Продолжает стилизацию десктопного хедера, описывает открытие/закрытие по клику на кнопку меню

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,24 @@
 'use strict';
 var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+var headerToggle = document.querySelector('.main-nav__toggle');
+var mainNav = document.querySelector('.main-nav');
 
 pageHeader.classList.remove('page-header--nojs');
 
 headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
+  if (pageHeader.classList.contains('page-header--nav-closed')) {
+    pageHeader.classList.remove('page-header--nav-closed');
+    headerToggle.classList.remove('main-nav__toggle--closed');
+    mainNav.classList.remove('main-nav--closed');
+    pageHeader.classList.add('page-header--nav-opened');
+    headerToggle.classList.add('main-nav__toggle--opened');
+    mainNav.classList.add('main-nav--opened');
   } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+    pageHeader.classList.add('page-header--nav-closed');
+    headerToggle.classList.add('main-nav__toggle--closed');
+    mainNav.classList.add('main-nav--closed');
+    pageHeader.classList.remove('page-header--nav-opened');
+    headerToggle.classList.remove('main-nav__toggle--opened');
+    mainNav.classList.remove('main-nav--opened');
   }
 });
